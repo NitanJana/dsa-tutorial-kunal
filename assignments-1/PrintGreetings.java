21cfe3ee381c164a5b7814c0bff1575cc56889ef
NitanJana <jananitan@gmail.com>
2022-08-30 18:13:19 +0530

    PrintGreetings program added

---
+++ assignments-1/PrintGreetings.java
@@ -0,0 +1,12 @@
+package Basic;/*Take name as input and print a greeting message for that particular name.*/
+import java.util.Scanner;
+
+public class _2_PrintGreetings {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+        System.out.println("Hi, "+name);
+        input.close();
+    }
+}
