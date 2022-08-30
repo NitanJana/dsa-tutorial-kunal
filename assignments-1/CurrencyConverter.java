8560c052a088aa97e32adc38540dec6443b647ee
NitanJana <jananitan@gmail.com>
2022-08-30 18:16:02 +0530

    CurrencyConverter program added

---
+++ assignments-1/CurrencyConverter.java
@@ -0,0 +1,15 @@
+/*Input currency in rupees and output in USD.*/
+package Basic;
+
+import java.util.Scanner;
+
+public class _6_CurrencyConverter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the value in inr: ");
+        float inr = input.nextFloat();
+        float usd = inr / 80;
+        System.out.println("The USD value of provided inr is : "+usd);
+        input.close();
+    }
+}
