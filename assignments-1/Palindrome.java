811cd02f439d6a1135cb8d5bcdaa844ae8ddda60
NitanJana <jananitan@gmail.com>
2022-08-30 18:17:09 +0530

    Palindrome program added

---
+++ assignments-1/Palindrome.java
@@ -0,0 +1,25 @@
+/*To find out whether the given String is Palindrome or not.*/
+package Basic;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class _8_Palindrome {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string to check if it is palindrome:");
+        String str = input.next();
+        StringBuilder rev = new StringBuilder();
+
+        for (int i = str.length()-1; i >=0 ; i--) {
+            rev.append(str.charAt(i));
+        }
+
+        if(Objects.equals(str, rev.toString())) {
+            System.out.println("The string is palindrome");
+        } else {
+            System.out.println("The string is not palindrome");
+        }
+        input.close();
+    }
+}
