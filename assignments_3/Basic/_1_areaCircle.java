8f2bed8fab01b7a51542b17f4ff8f6c4c5af9b9f
NitanJana <jananitan@gmail.com>
2022-08-31 17:02:49 +0530

    added area of triangle program

---
+++ assignments_3/Basic/_1_areaCircle.java
@@ -0,0 +1,17 @@
+/* Java program to calculate area of circle */
+
+import java.util.Scanner;
+
+public class _1_areaCircle {
+    private static final double PI = 3.14;
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the radius of the circle: ");
+        int radius = sc.nextInt();
+        System.out.println("The area of Circle is: "+(PI * radius * radius));
+
+        sc.close();
+    }
+}
---
+++ assignments_3/Basic/_2_areaTriangle.java
@@ -0,0 +1,18 @@
+/* Java program to calculate the area of Triangle*/
+
+import java.util.Scanner;
+
+public class _2_areaTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the base of the triangle: ");
+        int base = sc.nextInt();
+        System.out.print("Enter the height of the triangle: ");
+        int height = sc.nextInt();
+
+        System.out.println("The area of the circle is: "+(base * height * 0.5));
+
+        sc.close();
+    }
+}
